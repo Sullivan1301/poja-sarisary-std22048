a95bd763759ced300caf32d2a569d1dc4c384f48
Sullivan1301 <jorosullivan13@gmail.com>
2024-02-22 12:51:01 +0300

    fix: byte and file on sarisary service

--- src/main/java/hei/school/sarisary/service/SarisaryService.java
+++ src/main/java/hei/school/sarisary/service/SarisaryService.java
@@ -1,7 +1,5 @@
 import hei.school.sarisary.file.BucketComponent;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -10,53 +8,55 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SarisaryService {
 
-    private final BucketComponent bucketComponent;
+  private final BucketComponent bucketComponent;
 
-    @Autowired
-    public SarisaryService(BucketComponent bucketComponent) {
-        this.bucketComponent = bucketComponent;
+  @Autowired
+  public SarisaryService(BucketComponent bucketComponent) {
+    this.bucketComponent = bucketComponent;
+  }
+
+  public void processImage(byte[] imageBytes, String id) throws IOException {
+    String originalImageKey = "original/" + id;
+    bucketComponent.upload(imageBytes, originalImageKey);
+
+    byte[] transformedImageBytes = transformToBlackAndWhite(imageBytes);
+
+    String transformedImageKey = "transformed/" + id;
+    bucketComponent.upload(transformedImageBytes, transformedImageKey);
+  }
+
+  public Map<String, String> getImageUrls(String id) {
+    String originalImageKey = "original/" + id;
+    String transformedImageKey = "transformed/" + id;
+
+    URL originalImageUrl = bucketComponent.presign(originalImageKey, Duration.ofMinutes(10));
+    URL transformedImageUrl = bucketComponent.presign(transformedImageKey, Duration.ofMinutes(10));
+
+    Map<String, String> urls = new HashMap<>();
+    urls.put("original_url", originalImageUrl.toString());
+    urls.put("transformed_url", transformedImageUrl.toString());
+
+    return urls;
+  }
+
+  private byte[] transformToBlackAndWhite(byte[] imageBytes) throws IOException {
+    try (ByteArrayInputStream inputStream = new ByteArrayInputStream(imageBytes);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+      BufferedImage image = ImageIO.read(inputStream);
+
+      BufferedImage blackAndWhiteImage =
+          new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+      blackAndWhiteImage.getGraphics().drawImage(image, 0, 0, null);
+
+      ImageIO.write(blackAndWhiteImage, "png", outputStream);
+
+      return outputStream.toByteArray();
     }
-
-    public void processImage(byte[] imageBytes, String id) throws IOException {
-        String originalImageKey = "original/" + id;
-        bucketComponent.upload(imageBytes, originalImageKey);
-
-        byte[] transformedImageBytes = transformToBlackAndWhite(imageBytes);
-
-        String transformedImageKey = "transformed/" + id;
-        bucketComponent.upload(transformedImageBytes, transformedImageKey);
-    }
-
-    public Map<String, String> getImageUrls(String id) {
-        String originalImageKey = "original/" + id;
-        String transformedImageKey = "transformed/" + id;
-
-        URL originalImageUrl = bucketComponent.presign(originalImageKey, Duration.ofMinutes(10));
-        URL transformedImageUrl = bucketComponent.presign(transformedImageKey, Duration.ofMinutes(10));
-
-        Map<String, String> urls = new HashMap<>();
-        urls.put("original_url", originalImageUrl.toString());
-        urls.put("transformed_url", transformedImageUrl.toString());
-
-        return urls;
-    }
-
-    private byte[] transformToBlackAndWhite(byte[] imageBytes) throws IOException {
-        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(imageBytes);
-             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            BufferedImage image = ImageIO.read(inputStream);
-
-            BufferedImage blackAndWhiteImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
-            blackAndWhiteImage.getGraphics().drawImage(image,  0,  0, null);
-
-            ImageIO.write(blackAndWhiteImage, "png", outputStream);
-
-            return outputStream.toByteArray();
-        }
-    }
+  }
 }
